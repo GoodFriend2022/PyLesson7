0b8c18a3db3fb35838269b45d64695da5854303e
Uri_Klevcov <a-08-137.mail.ru>
2023-01-23 00:46:36 +0600

    project

--- function.py
+++ function.py
@@ -8,7 +8,7 @@ def read_data_from_file(filename):
 
 def add_to_file(filename, data_list, do = 'a'):
     with open(filename, do, encoding='utf8') as datafile:
-        datafile.write(data_list[0] + ', '.join([input(i) for i in data_list if i != data_list[0]]))
+        datafile.write(data_list[0] + ', '.join([input(i) for i in data_list if i != data_list[0]]) + '\n')
 
 show = lambda filename: print(read_data_from_file(filename))
 
@@ -29,34 +29,67 @@ def invers_route(file_way = 'temp.txt', file_bus = 'buses.txt', file_driver = 'd
     route = read_data_from_file(file_way)
     bus = read_data_from_file(file_bus)
     driver = read_data_from_file(file_driver)
-    for i in route:
-        for id in bus:
-            if i[2].lower() == id[1].lower():
-                i[2] = id[0]
-        for id in driver:
-            if i[3].lower() == id[1].lower():
-                i[3] = id[0]
+    item_bus = route[0][2]
+    item_driver = route[0][3]
+    for id in bus:
+        if route[0][2].lower() == id[1].lower():
+            route[0][2] = id[0]
+    if route[0][2] == item_bus:
+        if input('Такой автобус до настоящего момента в автопарке отсутствовал. Добавить?' +
+            '\nY/N >>> ').lower() == 'y':
+            id_bus = int(bus[len(bus) - 1][0].strip('bus')) + 1
+            with open(file_bus, 'a', encoding='utf8') as datafile:
+                datafile.write(f'bus{id_bus}, {route[0][2]}\n')
+            route[0][2] = f'bus{id_bus}'
+        else: return False
+    for id in driver:
+        if route[0][3].lower() == id[1].lower():
+            route[0][3] = id[0]
+    if route[0][3] == item_driver:
+        if input('Такой водитель в списке сотрудников отсутствует. Добавить?' +
+            '\nY/N >>> ').lower() == 'y':
+            id_driver = int(driver[len(driver) - 1][0].strip('driver')) + 1
+            with open(file_driver, 'a', encoding='utf8') as datafile:
+                datafile.write(f'driver{id_driver}, {route[0][3]}\n')
+            route[0][2] = f'driver{id_driver}'
+        else: return False   
     return route
 
-def find_route(filename)
+def find_object(filename, ask):
+    user_object = input(ask).lower()
+    if filename == 'way.txt':
+        objects = convert_route(filename)
+    else: objects = read_data_from_file(filename)
+    object = [i for i in objects if user_object == i[1].lower()]
+    if object == []:
+        return 'Искомого объекта не найдено'
+    else: return object
 
-def del_route(filename):
-    
-
-
-
-    person = FindName(filename)
-    ask = input('Удалить абонента? \n(Y/N) > ')
-    if ask.lower() == 'y':
-        with open(filename, 'r+') as data:
-            for line in data:
-                if list(line.split(', ')) != person:
-                    with open('temp.txt', 'a') as data:
-                        data.write(line)
-        os.remove(filename)
-        os.rename('temp.txt', filename)
-    return
+def del_object(filename, ask):
+    object = find_object(filename, ask)
+    if type(object) != list:
+        return object
+    else:
+        ask = input('Вы уверены? \n(Y/N) >>> ')
+        if ask.lower() == 'y':
+            objects = read_data_from_file(filename)
+            new_objects = [i for i in objects if i[0] != object[0][0]]
+            with open(filename, 'w', encoding='utf8') as datafile:
+                for i in new_objects:
+                    datafile.write(', '.join(i) + '\n')
+        return f'{object} \t объект удален'
 
 bus = ['bus', 'Введите id автобуса: ', 'А также гос. номер автобуса: ']
 driver = ['driver', 'Введите id водителя: ', 'А также его фамилию: ']
 route = ['m', 'Введите id маршрута: ', 'А также его номер: ', 'Введите гос номер автобуса: ', 'Введите фамилию водителя: ']
+menu_find = [
+    ("1", "Найти автобус"),
+    ("2", "Найти водителя"),
+    ("3", "Найти маршрут"),
+    ("4", "Выход")]
+menu_del = [
+    ("1", "Удалить автобус"),
+    ("2", "Удалить водителя"),
+    ("3", "Удалить маршрут"),
+    ("4", "Выход")]
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # from modul_menu import Menu
 import function as f
+import os
 
 # if __name__ == "__main__":
 menu_items = [
@@ -9,29 +10,90 @@ menu_items = [
     ("4", "Запись водителей"),
     ("5", "Вывод маршрута"),
     ("6", "Запись маршрута"),
-    ("7", "Удалить маршрут")
-    ("8", "Выход")]
+    ("7", "Поиск"),
+    ("8", "Удаление"),
+    ("9", "Выход")]
 
 # menu = Menu(menu_items)
 # menu.run('>: ')
 
-for i in menu_items:
-    print(' - '.join(i))
-user_text = input('Введите номер меню: ')
-if user_text == '1':
-    f.show('buses.txt')
-elif user_text == '2':
-    f.add_to_file('buse.txt', f.bus)
-elif user_text == '3':
-    f.show('drivers.txt')
-elif user_text == '4':
-    f.add_to_file('drivers.txt', f.driver)
-elif user_text == '5':
-    print(f.convert_route())
-elif user_text == '6':
-    f.add_to_file('temp.txt', f.route, 'w')
-    with open('way.txt', 'a', encoding='utf8') as datafile:
-        datafile.write(', '.join(f.invers_route()[0]))
+os.system('cls')
+while True:
+    for i in menu_items:
+        print(' - '.join(i))
+    user_text = input('Введите номер меню: ')
+    if user_text == '1':
+        os.system('cls')
+        f.show('buses.txt')
+    elif user_text == '2':
+        f.add_to_file('buses.txt', f.bus)
+        os.system('cls')
+    elif user_text == '3':
+        os.system('cls')
+        f.show('drivers.txt')
+    elif user_text == '4':
+        f.add_to_file('drivers.txt', f.driver)
+        os.system('cls')
+    elif user_text == '5':
+        os.system('cls')
+        print(f.convert_route())
+    elif user_text == '6':
+        f.add_to_file('temp.txt', f.route, 'w')
+        route = f.invers_route()
+        if type(route) == list:
+            with open('way.txt', 'a', encoding='utf8') as datafile:
+                datafile.write(', '.join(route[0]) + '\n')
+        os.system('cls')
+    elif user_text == '7':
+        os.system('cls')
+        while(True):
+            for i in f.menu_find:
+                print(' - '.join(i))
+            user_find = input('Введите номер меню: ')
+            if user_find == '1':
+                os.system('cls')
+                print(f.find_object('buses.txt', 'Введите номер автобуса: '))
+            elif user_find == '2':
+                os.system('cls')
+                print(f.find_object('drivers.txt', 'Введите фамилию водителя: '))
+            elif user_find == '3':
+                os.system('cls')
+                print(f.find_object('way.txt', 'Введите номер маршрута: '))
+            elif user_find == '4':
+                os.system('cls')
+                break
+            else: 
+                os.system('cls')
+                print('Введенный номер в меню отсутствует')
+                break
+    elif user_text == '8':
+        os.system('cls')
+        while(True):
+            for i in f.menu_del:
+                print(' - '.join(i))
+            user_del = input('Введите номер меню: ')
+            if user_del == '1':
+                os.system('cls')
+                print(f.del_object('buses.txt', 'Введите номер автобуса: '))
+            elif user_del == '2':
+                os.system('cls')
+                print(f.del_object('drivers.txt', 'Введите фамилию водителя: '))
+            elif user_del == '3':
+                os.system('cls')
+                print(f.del_object('way.txt', 'Введите номер маршрута: '))
+            elif user_del == '4':
+                os.system('cls')
+                break
+            else: 
+                os.system('cls')
+                print('Введенный номер в меню отсутствует')
+                break
+    elif user_text == '9':
+        print('CHAO')
+        break
+    else: 
+        print('Введенный номер в меню отсутствует')
+        break
 
 
 
